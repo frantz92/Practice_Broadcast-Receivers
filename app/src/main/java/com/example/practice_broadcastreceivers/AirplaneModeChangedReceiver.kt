5556b8ea7c61ffa5f1943e2be02977d614b34f3b
Patryk Frankowski <patryk.frankowski@capgemini.com>
2021-06-27 18:48:11 +0200

    Add code

---
+++ app/src/main/java/com/example/practice_broadcastreceivers/AirplaneModeChangedReceiver.kt
@@ -0,0 +1,18 @@
+package com.example.practice_broadcastreceivers
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AirplaneModeChangedReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val isAirplaneModeEnabled = intent?.getBooleanExtra("state", false) ?: return
+        if(isAirplaneModeEnabled) {
+            Toast.makeText(context, "Airplane Mode Enabled", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(context, "Airplane Mode Disabled", Toast.LENGTH_LONG).show()
+        }
+    }
+}
